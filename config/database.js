c4c8fb8b076929d4ea226c2c6c8b27a4baeae9e2
Randy Elkins <randy.e.elkins@gmail.com>
2021-02-05 15:41:48 -0800

    update config/database.js for MongoDB Atlas use

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-// const connectionStr = process.env.DATABASE_URL; // ‘camps’ is the name of the DB that will be either:
-const connectionStr = 'mongodb://127.0.0.1:27017/goCampBlog'; // ‘camps’ is the name of the DB that will be either:
+const connectionStr = process.env.DATABASE_URL; // ‘camps’ is the name of the DB that will be either:
+// const connectionStr = 'mongodb://127.0.0.1:27017/goCampBlog'; // ‘camps’ is the name of the DB that will be either:
 // - created, if it doesn’t already exist, or
 // - connected to, if it exists
 
